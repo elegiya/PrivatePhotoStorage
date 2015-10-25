cbcc220b4d70bd03de212dcfb2bb56a4f5bd448d
Marina Gaponova <marina.a.gaponova@gmail.com>
2015-10-26 00:27:27 +0200

    Primary app version

---
+++ PrivatePhotoStorage/PrivatePhotoStorage/PrivatePhotoStorage.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PrivatePhotoStorage.WinPhone.Resources;
+
+namespace PrivatePhotoStorage.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
